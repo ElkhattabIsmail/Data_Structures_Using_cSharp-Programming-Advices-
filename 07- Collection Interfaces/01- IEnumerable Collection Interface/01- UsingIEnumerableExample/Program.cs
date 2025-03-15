a4153dad3ac54547283adc751c8d2aada6e5602e
ELKHATTAB SMAIL <elhattab207@gmail.com>
2025-03-15 15:01:09 +0000

    Collection Interfaces.

---
+++ 07- Collection Interfaces/01- IEnumerable Collection Interface/01- UsingIEnumerableExample/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+
+namespace CustomCollectionExample
+{
+    
+    // MoveNext() → returns false (no more items)
+    public class CustomCollection<T> : IEnumerable<T> // Methods Must Impement from the User that inherits from this interface.
+    {
+        //  You can use any dataStructure to help u to implement these Methods 
+        //  Lists, arrays, sets, HashSet, Dictionary, stack , queue.....
+        private List<T> items = new List<T>();
+
+        // M1
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                yield return items[i];  // Yield ==> Store Last Item Returned.
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 10, 20, 30};
+            var enumerator = list.GetEnumerator();
+
+            foreach (var item in list)
+            {
+
+                enumerator.MoveNext();
+                Console.WriteLine(enumerator.Current);
+
+            }
+            Console.ReadKey();
+
+
+            var myCollection = new CustomCollection<int>();
+            myCollection.Add(1);
+            myCollection.Add(2);
+            myCollection.Add(3);
+
+
+            foreach (var item in myCollection)
+            {
+                Console.WriteLine(item);
+            }
+
+        
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 07- Collection Interfaces/02- ICollection - Collection Interface/02- Implementing ICollection Based On Arrays/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections;
+
+
+namespace ICollectionExample
+{
+  
+    public class SimpleCollectionBasedOnArray<T> : ICollection<T>  // ICollection Hight Level Interface than IEnumrable.
+    {
+
+        private T[] Arr = new T [1];
+        private T[] CopyArr = new T[1];
+
+        public int Count => _ArrayLen ;
+        public bool IsReadOnly => false;
+
+        private int _ArrayLen = 0;
+        public void Add(T item)
+        {
+            _ArrayLen++;
+            T[] TempArr = new T[_ArrayLen];
+
+            for (int i = 0; i < _ArrayLen; i++)
+            {
+                TempArr[i] = Arr[i];
+            }
+
+            TempArr[_ArrayLen - 1] = item;
+
+            Arr = TempArr;
+            CopyArr = new T[_ArrayLen + 1];
+
+            for (int i = 0; i < Arr.Length; i++) // for increasing the origin array.
+            {
+                CopyArr[i] = Arr[i];
+            }
+            Arr = CopyArr;
+        }
+        public void SetValueAtIndex(T item , int Index)
+        {
+            if (Index < Count)
+            {
+                Arr.SetValue(item, Index);
+            }
+        }
+        public void Clear()
+        {
+            _ArrayLen = 0;
+            Arr = new T[1];
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < Count; i++) // Cuz Last Items equal 0 by defaut.
+            {
+                if (item.Equals(Arr[i]))
+                    return true;
+            }
+            return false;
+        }
+
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Array.Copy(Arr, array, arrayIndex - 1);
+        }
+
+
+        public bool Remove(T item)// Equals Method: The x.Equals(item) ensures proper comparison
+                                  // for objects of type T. 
+        {
+            if (Arr.Contains(item))
+            {
+                Arr = Arr.Where(x => !x.Equals(item) ).ToArray();// compare two objects.
+                _ArrayLen--;
+                return true;
+            }
+
+            return false;
+        }
+
+        // IEnumrable Interface would you Implement it.
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return Arr[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleCollectionBasedOnArray<int> MyCollection = new SimpleCollectionBasedOnArray<int>();
+            MyCollection.Add(1);
+            MyCollection.Add(2);
+            MyCollection.Add(3);
+            MyCollection.SetValueAtIndex(4, 3);
+            MyCollection.SetValueAtIndex(5, 4);
+
+
+            int[] TempArr = new int[3];
+            MyCollection.CopyTo(TempArr,4);
+
+            if (MyCollection.Contains(0))
+                Console.WriteLine("\nItem Exist.");
+            else
+                Console.WriteLine("\nItem Not Exist.");
+
+            Console.WriteLine("All Number in the Collection:");
+            foreach (var item in MyCollection)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nCollection Count = " + MyCollection.Count);
+            MyCollection.Remove(2);
+            Console.WriteLine("Collection After Removing Number 2 :");
+            foreach (var item in MyCollection)
+            {
+                Console.WriteLine(item);
+            }
+            MyCollection.Clear();
+            Console.WriteLine("\nCollection Count After Clear it = " + MyCollection.Count);
+            MyCollection.Add(1);
+            Console.WriteLine("\nCollection Count After Readding New Value = " + MyCollection.Count);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
